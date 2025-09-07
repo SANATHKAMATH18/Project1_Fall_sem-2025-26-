77b24f48b5c1e60b5e53700c9c068287311712ec
SANATHKAMTH18 <sanathkamath11@gmail.com>
2025-09-08 00:05:58 +0530

    fixed bugs

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "c:/Users/sanat/OneDrive/Desktop/Project1_fall_sem25_26"
 start "Streamlit" cmd /k "streamlit run frontend.py"
-start "Main" cmd /k "python fastapi.py"
+start "Main" cmd /k "python api.py"
